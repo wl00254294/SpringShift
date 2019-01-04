39a56ccad83d94c9edb538b3a48a24eca6c45d58
EricWu <wl00254294@gmail.com>
2019-01-04 10:38:02 +0800

    init

---
+++ src/main/java/com/example/demo/controller/HellowController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HellowController {
+	@RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
