c353594aff7739d632850b5ac8ffccbb29549d4a
wl00254294 <43841531+wl00254294@users.noreply.github.com>
2019-01-04 15:33:58 +0800

    Update HellowController.java

--- src/main/java/com/example/demo/controller/HellowController.java
+++ src/main/java/com/example/demo/controller/HellowController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HellowController {
-	@RequestMapping("/")
+	@RequestMapping("/demo")
     public String index() {
         return "Greetings from Spring Boot!";
     }
