1654dd50c4b9f7b57566d37255f2ad67d67b7e77
wl00254294 <43841531+wl00254294@users.noreply.github.com>
2019-01-09 11:22:01 +0800

    Update SpringShiftApplication.java

--- src/main/java/com/example/demo/SpringShiftApplication.java
+++ src/main/java/com/example/demo/SpringShiftApplication.java
@@ -1,12 +1,21 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringShiftApplication {
+public class SpringShiftApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringShiftApplication.class);
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringShiftApplication.class, args);
 	}
